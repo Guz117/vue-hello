f8998e29b07d8dabaa48a7b67835c72f926c69a1
Guz117 <cristian.guzman117@gmail.com>
2021-12-15 15:04:04 +0100

    messaggio e immagine

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 const app = new Vue({
     el: '#app',
     data: {
-      
+        message: 'Benvenuto nella mia vita Vue',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
     }
   })
 
